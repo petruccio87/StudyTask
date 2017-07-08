c498940d476954577ffd513ce8e85f6cd9d7d49b
OSX <osx@macbook-pro-osx.local>
2017-07-08 14:14:04 +0300

    Les5 - Сделана загрузка инфо от api google places и разбор получаемого json по полям.

---
+++ Pods/Target Support Files/Pods-les5/Pods-les5-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_les5 : NSObject
+@end
+@implementation PodsDummy_Pods_les5
+@end
--- les5/ViewController.swift
+++ les5/ViewController.swift
@@ -2,17 +2,57 @@
 //  ViewController.swift
 //  les5
 //
-//  Created by OSX on 05.07.17.
+//  Created by Petr Shibalov on 05.07.17.
 //  Copyright © 2017 OSX. All rights reserved.
 //
 
 import UIKit
+
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let apikey = "AIzaSyCzZGMY1lCgjzX1aVAEGXluHrKS19YDr4w"
+        let latlng = "55.761704,37.620350"
+        let radius = "150"
+        let rankby = "distance"
+        let placeType = "bar"
+        let language = "ru"
+        
+        let urlByRadius = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+latlng+"&radius="+radius+"&opennow=true&type="+placeType+"&language="+language+"&key=" + apikey
+        let urlByDistance = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+latlng+"&rankby="+rankby+"&opennow=true&type="+placeType+"&language="+language+"&key=" + apikey
+        
+        Alamofire.request(urlByDistance, method: .get).validate().responseJSON { response in
+            switch response.result{
+                case .success(let value):
+                    let json = JSON(value)
+                    if json["status"].stringValue == "OK" {
+                        for (key,place):(String, JSON) in json["results"] {
+                            print(key, " ", place["name"].stringValue)
+                            print("     Rating: ", place["rating"].stringValue)
+                            print("     Price Level: ", place["price_level"].stringValue)   // не везде есть
+                            print("     LatLng: ", place["geometry"]["location"]["lat"].stringValue, ",", place["geometry"]["location"]["lng"].stringValue)
+                            print("     Адрес: ", place["vicinity"].stringValue)
+                        }
+                    }
+                    //print("Json ResponseResult: \(json)")
+                case .failure(let error):
+                    print(error)
+            }
+        /*    print(response.request)
+            print(response.response) // HTTP URL response
+            print(response.data)     // server data
+            print(response.result)   // result of response serialization
+            
+            if let JSON = response.result.value {
+                print("JSON: \(JSON)")
+            } */
+        }
     }
 
     override func didReceiveMemoryWarning() {
